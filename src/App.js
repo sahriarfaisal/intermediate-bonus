5e528854d6373d726b6f9e51ecb9e42d52d53ad9
sahriarfaisal.cse@gmail.com <sahriarfaisal.cse@gmail.com>
2020-11-05 20:29:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { useState } from 'react';
 function App() {
+  const [likeColor, setLikeColor] = useState('')
+  const handleLike = () =>{
+    const color = likeColor ? '' : 'primary';
+    setLikeColor(color)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AccessAlarmIcon></AccessAlarmIcon>
+      <ThumbUpAltIcon onClick={handleLike} color={likeColor}></ThumbUpAltIcon>
     </div>
   );
 }
